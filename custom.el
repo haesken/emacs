36ecc3cb376e310c2678ac8fddced313a72c66fc
haesken08 <haesken08@gmail.com>
2014-07-20 04:55:09 -0500

    add custom.el

---
+++ custom.el
@@ -0,0 +1,35 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(font-lock-builtin-face ((t (:weight bold))))
+ '(font-lock-comment-face ((t (:weight bold))))
+ '(font-lock-constant-face ((t (:weight bold))))
+ '(font-lock-doc-face ((t (:weight bold))))
+ '(font-lock-function-name-face ((t (:weight bold))))
+ '(font-lock-keyword-face ((t (:weight bold))))
+ '(font-lock-preprocessor-face ((t (:weight bold))))
+ '(font-lock-regexp-grouping-backslash ((t (:weight bold))))
+ '(font-lock-regexp-grouping-construct ((t (:weight bold))))
+ '(font-lock-string-face ((t (:weight bold))))
+ '(font-lock-type-face ((t (:slant normal :weight bold))))
+ '(font-lock-variable-name-face ((t (:weight bold))))
+ '(font-lock-warning-face ((t (:slant normal :weight bold))))
+ '(linum-relative-current-face ((t (:foreground "#f8f8f2" :weight bold))))
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "#fd971f"))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "#a6e22e"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "#a1efe4"))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "#fd971f"))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "#a6e22e"))))
+ '(rainbow-delimiters-depth-6-face ((t (:foreground "#a1efe4"))))
+ '(rainbow-delimiters-depth-7-face ((t (:foreground "#fd971f"))))
+ '(rainbow-delimiters-depth-8-face ((t (:foreground "#a6e22e"))))
+ '(rainbow-delimiters-depth-9-face ((t (:foreground "#a1efe4"))))
+)
