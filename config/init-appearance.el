be901d777c68d621b9b399879c6a9aaa4ef81886
haesken08 <haesken08@gmail.com>
2014-07-21 06:56:12 -0500

    Remove duplicate commands to disable gui

--- config/init-appearance.el
+++ config/init-appearance.el
@@ -7,11 +7,6 @@
 (add-to-list 'initial-frame-alist '(font . "Deja Vu Sans Mono:weight=bold"))
 (add-to-list 'default-frame-alist '(font . "Deja Vu Sans Mono:weight=bold"))
 
-;; Disable menu, tool, & scroll bars
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-
 ;; Highlight matching parens
 (show-paren-mode 1)
 (setq show-paren-delay 0)
