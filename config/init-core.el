019a60bc18e8d66b1d566998f54278fbe386c74d
haesken08 <haesken08@gmail.com>
2014-07-21 07:23:05 -0500

    Fix comments, fix tabs, enable spell check in some modes.

--- config/init-core.el
+++ config/init-core.el
@@ -1,4 +1,4 @@
-;; store most files in the cache
+;; Store most files in the cache
 (setq backup-directory-alist
       `((".*" . ,(concat dotemacs-cache-directory "backups")))
       auto-save-file-name-transforms
@@ -6,32 +6,59 @@
       auto-save-list-file-prefix
       (concat dotemacs-cache-directory "auto-save-list/saves-"))
 
-;; better scrolling
+;; Better scrolling
 (setq scroll-conservatively 9999
       scroll-preserve-screen-position t)
 
 (defalias 'yes-or-no-p 'y-or-n-p)
 
-;; Set encoding to utf8
+;; Set default encoding to utf8
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 (set-selection-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
 
-(setq indent-tabs-mode nil) ; And force use of spaces
-(setq c-basic-offset 4) ; indents 4 chars
-(setq tab-width 4)          ; and 4 char wide for TAB
+;; Use spaces not tabs
+(setq indent-tabs-mode nil)
+
+;; Default indent width 4 chars
+(setq tab-width 4) ;; Show tabs as 4 spaces
 (setq-default tab-width 4)
 (setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60
                       64 68 72 76 80 84 88 92 96 100 104 108 112
                       116 120))
-(define-key text-mode-map (kbd "TAB") 'tab-to-tab-stop)
 
+; Stop doing crazy indent shit
+;; Global
+(global-set-key (kbd "TAB") 'tab-to-tab-stop)
+;; Per Mode
+;; (define-key text-mode-map (kbd "TAB") 'tab-to-tab-stop)
+
+
+;; No bells
 (setq ring-bell-function (lambda () ()))
+;; Don't make lock files
 (setq create-lockfiles nil)
 
 (setq inhibit-splash-screen t)
 (setq inhibit-startup-echo-area-message t)
 (setq inhibit-startup-message t)
+
+;; Enable ido
+(require 'ido)
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode t)
+
+;; Display ido results vertically
+(require-package 'ido-vertical-mode)
+(require 'ido-vertical-mode)
+(ido-vertical-mode 1)
+
+;; Enable spell checking for text-mode buffers
+(require 'flyspell)
+(add-hook 'text-mode-hook 'flyspell-mode)
+(add-hook 'text-mode-hook 'flyspell-buffer)
+(setq flyspell-issue-message-flag nil)
 
 (provide 'init-core)
--- config/init-markdown.el
+++ config/init-markdown.el
@@ -1,4 +1,10 @@
-(lazy-major-mode "\\.md$" markdown-mode)
+(require-package 'markdown-mode)
+(require 'markdown-mode)
+
+(autoload 'markdown-mode "markdown-mode"
+   "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
 (provide 'init-markdown)
 
