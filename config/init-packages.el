afa4163fe564f26f311d1836e143d6d02e828254
haesken08 <haesken08@gmail.com>
2014-07-21 07:23:30 -0500

    remove old package func

--- config/init-packages.el
+++ config/init-packages.el
@@ -12,15 +12,4 @@
       (package-refresh-contents))
     (package-install package)))
 
-;; (add-to-list 'load-path (concat user-emacs-directory "el-get/el-get"))
-;; (unless (require 'el-get nil 'noerror)
-;;   (with-current-buffer
-;;       (url-retrieve-synchronously
-;;        "https://github.com/dimitri/el-get/raw/master/el-get-install.el")
-;;     (let (el-get-master-branch)
-;;       (goto-char (point-max))
-;;       (eval-print-last-sexp))))
-;; (el-get 'sync)
-
-
 (provide 'init-packages)
