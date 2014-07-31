eb7f1d9373cbd102d54e3f0ca04a8cfb02ac9cfa
haesken08 <haesken08@gmail.com>
2014-07-31 05:21:22 -0500

    rearrage evil init a bit, add evil-surround

--- config/init-evil.el
+++ config/init-evil.el
@@ -9,23 +9,26 @@
 (setq evil-operator-state-cursor '(monokai-fg box))
 
 (require-package 'evil)
-(require-package 'evil-leader)
-(require-package 'evil-indent-textobject)
-;; (require-package 'evil-matchit)
 
+(require-package 'evil-leader)
 (require 'evil-leader)
-(global-evil-leader-mode)
+(global-evil-leader-mode t)
 (evil-leader/set-leader ",")
 
 (require-package 'evil-nerd-commenter)
 (require 'evil-nerd-commenter)
 
+(require-package 'evil-surround)
+(require 'evil-surround)
+(global-evil-surround-mode 1)
+
 (setq evil-shift-width 4)
 (require 'evil)
+(require-package 'evil-indent-textobject)
 (require 'evil-indent-textobject)
+
 (evil-mode t)
 
-;; (add-hook 'term-mode-hook 'evil-insert-state)
 (add-hook 'term-mode-hook 'evil-emacs-state)
 
 (defadvice evil-ex-search-next (after advice-for-evil-ex-search-next activate)
