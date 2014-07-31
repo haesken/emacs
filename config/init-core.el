386e69fdd9a383ce4c7d0e33446c4d4ca40b703d
haesken08 <haesken08@gmail.com>
2014-07-31 05:20:43 -0500

    seriously use utf-8-unix

--- config/init-core.el
+++ config/init-core.el
@@ -17,13 +17,14 @@
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 (set-selection-coding-system 'utf-8)
-;; (prefer-coding-system 'utf-8)
 
 (set-default buffer-file-coding-system 'utf-8-unix)
 (set-default-coding-systems 'utf-8-unix)
-(set-default default-buffer-file-coding-system 'utf-8-unix)
-
+;; (set-default file-co'utf-8-unix)
+;; (prefer-coding-system 'utf-8)
 (prefer-coding-system 'utf-8-unix)
+(setq coding-system-for-read 'utf-8-unix)
+(setq coding-system-for-write 'utf-8-unix)
 
 ;; Use spaces not tabs
 (setq-default indent-tabs-mode nil)
