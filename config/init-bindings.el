f809cff253c3f54b60a3d8ecfc52d5ed94fbce4c
haesken08 <haesken08@gmail.com>
2015-06-24 09:05:52 -0500

    add more bindings for navigation completion results

--- config/init-bindings.el
+++ config/init-bindings.el
@@ -157,4 +157,8 @@
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 (setq helm-locate-fuzzy-match t)
 
+;; Auto Complete
+(define-key ac-complete-mode-map "\C-n" 'ac-next)
+(define-key ac-complete-mode-map "\C-p" 'ac-previous)
+
 (provide 'init-bindings)
