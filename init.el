fbe5c7e78eb175620b1843a0cfaf17b5e9560414
haesken08 <haesken08@gmail.com>
2015-06-24 09:09:08 -0500

    add auto compile

--- init.el
+++ init.el
@@ -26,6 +26,13 @@
 (when (file-exists-p custom-file)
     (load custom-file))
 
+;; Auto compile
+(setq load-prefer-newer t)
+(require-package 'auto-compile)
+(require 'auto-compile)
+(auto-compile-on-load-mode 1)
+(auto-compile-on-save-mode 1)
+
 ;; --- Global ---
 (require 'init-core)
 (require 'init-indent)
