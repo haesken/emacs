9e4957d5d24a8db8b02eaed689a7b8af59b9a0ba
haesken08 <haesken08@gmail.com>
2015-07-01 02:53:09 -0500

    add modes to autocomplete in their own config, set lua indent pref

--- config/init-lua.el
+++ config/init-lua.el
@@ -1,10 +1,14 @@
 (require-package 'lua-mode)
 (require 'lua-mode)
 
-(eval-after-load "auto-complete" '(add-to-list 'ac-modes 'lua-mode))
-
 (autoload 'lua-mode "lua-mode" "Lua editing mode." t)
 (add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
 (add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
+
+(eval-after-load "auto-complete" '(add-to-list 'ac-modes 'lua-mode))
+
+(add-hook 'lua-mode-hook
+          (lambda ()
+            (setq lua-indent-level 4)))
 
 (provide 'init-lua)
