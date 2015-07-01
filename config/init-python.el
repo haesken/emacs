8e616cdcef4ca370b6088ab61e3b23a04d1ca688
haesken08 <haesken08@gmail.com>
2015-07-01 02:51:36 -0500

    setup auto fill for python

--- config/init-python.el
+++ config/init-python.el
@@ -20,4 +20,18 @@
 
 (ad-activate 'python-calculate-indentation)
 
+;; http://stackoverflow.com/questions/8823924/in-emacs-python-mode-how-do-i-set-a-different-auto-fill-width-for-docstrings-an/30340262#30340262
+(defun my-python-fill-string (&optional justify)
+  (let ((old-fill-column fill-column))
+    (setq fill-column 72)
+    (python-fill-string justify)
+    (setq fill-column old-fill-column)))
+
+(add-hook 'python-mode-hook
+          (lambda ()
+            (setq fill-column 80)
+            (setq python-fill-docstring-style 'django)
+            (setq python-fill-string-function my-python-fill-string)
+            (auto-fill-mode t)))
+
 (provide 'init-python)
